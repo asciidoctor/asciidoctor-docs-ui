5407d30f17410ae22a20090de2c8614795f47d68
Dan Allen <dan.j.allen@gmail.com>
2020-11-06 14:10:18 -0700

    integrate Algolia docsearch client

---
+++ src/js/vendor/docsearch.bundle.js
@@ -0,0 +1,19 @@
+;(function () {
+  'use strict'
+
+  var docsearch = require('docsearch.js/dist/cdn/docsearch.js')
+  var config = document.getElementById('search-script').dataset
+  var link = document.createElement('link')
+  link.rel = 'stylesheet'
+  link.href = config.stylesheet
+  document.head.appendChild(link)
+  var search = docsearch({
+    appId: config.appId,
+    apiKey: config.apiKey,
+    indexName: config.indexName,
+    inputSelector: '#search-input',
+    autocompleteOptions: { hint: false, keyboardShortcuts: ['s'] },
+    algoliaOptions: { hitsPerPage: 10 },
+  }).autocomplete
+  search.on('autocomplete:closed', function () { search.autocomplete.setVal() })
+})()
