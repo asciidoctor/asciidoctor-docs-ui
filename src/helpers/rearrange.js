0d94d6c1f23f0d78bd8cc39903352250ba2a1c11
Dan Allen <dan.j.allen@gmail.com>
2020-12-10 03:44:38 -0700

    fix logic in rearrange helper when using negated entry

--- src/helpers/rearrange.js
+++ src/helpers/rearrange.js
@@ -1,15 +1,16 @@
 'use strict'
 
 module.exports = (collection, property, orderSpec) => {
-  if (!orderSpec || orderSpec === '*') return Object.values(collection)
-  const order = orderSpec.split(',').map((it) => it.trim())
+  if (orderSpec == null || orderSpec === '*') return Object.values(collection)
   const sourceCollection = Object.values(collection).reduce((accum, it) => accum.set(it[property], it), new Map())
+  const order = orderSpec.split(',').map((it) => it.trim()).filter((it) => {
+    if (it.charAt() !== '!') return true
+    sourceCollection.delete(it.substr(1))
+  })
   const restIdx = order.indexOf('*')
   if (~restIdx) order.splice(restIdx, 1)
   const targetCollection = order.reduce((accum, key) => {
-    if (key.startsWith('!')) {
-      sourceCollection.delete(key.substr(1))
-    } else if (sourceCollection.has(key)) {
+    if (sourceCollection.has(key)) {
       accum.push(sourceCollection.get(key))
       sourceCollection.delete(key)
     }
