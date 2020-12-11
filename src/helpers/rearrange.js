e855dc8296ff53fd98200f94d11adc1fe032f992
Dan Allen <dan.j.allen@gmail.com>
2020-12-11 04:56:50 -0700

    format source

--- src/helpers/rearrange.js
+++ src/helpers/rearrange.js
@@ -3,10 +3,13 @@
 module.exports = (collection, property, orderSpec) => {
   if (orderSpec == null || orderSpec === '*') return Object.values(collection)
   const sourceCollection = Object.values(collection).reduce((accum, it) => accum.set(it[property], it), new Map())
-  const order = orderSpec.split(',').map((it) => it.trim()).filter((it) => {
-    if (it.charAt() !== '!') return true
-    sourceCollection.delete(it.substr(1))
-  })
+  const order = orderSpec
+    .split(',')
+    .map((it) => it.trim())
+    .filter((it) => {
+      if (it.charAt() !== '!') return true
+      sourceCollection.delete(it.substr(1))
+    })
   const restIdx = order.indexOf('*')
   if (~restIdx) order.splice(restIdx, 1)
   const targetCollection = order.reduce((accum, key) => {
--- src/helpers/siteStartPage.js
+++ src/helpers/siteStartPage.js
@@ -1,4 +1,9 @@
 'use strict'
 
-module.exports = ({ data: { root: { contentCatalog } } }) =>
-  contentCatalog && contentCatalog.getSiteStartPage()
+module.exports = ({
+  data: {
+    root: {
+      contentCatalog = { getSiteStartPage() {} },
+    },
+  },
+}) => contentCatalog.getSiteStartPage()
