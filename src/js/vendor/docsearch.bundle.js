8a679a35ae85a22da1aaf84b7500dbe2e05a22e6
Guillaume Grossetie <ggrossetie@gmail.com>
2020-12-21 13:46:38 +0100

    Add project (contextual) filter to search (PR #39)
    Co-authored-by: Dan Allen <dan.j.allen@gmail.com>

--- src/js/vendor/docsearch.bundle.js
+++ src/js/vendor/docsearch.bundle.js
@@ -1,31 +1,63 @@
 ;(function () {
   'use strict'
 
-  var docsearch = require('docsearch.js/dist/cdn/docsearch.js')
-  var config = document.getElementById('search-script').dataset
-  var link = document.createElement('link')
-  link.rel = 'stylesheet'
-  link.href = config.stylesheet
-  document.head.appendChild(link)
-  var search = docsearch({
-    appId: config.appId,
-    apiKey: config.apiKey,
-    indexName: config.indexName,
-    inputSelector: '#search-input',
-    autocompleteOptions: { hint: false, keyboardShortcuts: ['s'] },
-    algoliaOptions: { hitsPerPage: parseInt(config.maxResults) || 5 },
-  }).autocomplete
-  var autocomplete = search.autocomplete
-  search.on(
-    'autocomplete:updated',
-    function () {
-      this.scrollTop = 0
-    }.bind(autocomplete.getWrapper().firstChild)
-  )
-  search.on(
-    'autocomplete:closed',
-    function () {
-      this.setVal()
-    }.bind(autocomplete)
-  )
+  activateSearch(require('docsearch.js/dist/cdn/docsearch.js'), document.getElementById('search-script').dataset)
+
+  function activateSearch (docsearch, config) {
+    appendStylesheet(config.stylesheet)
+    var algoliaOptions = { hitsPerPage: parseInt(config.maxResults) || 15 }
+    var searchFieldSelector = '#' + (config.searchFieldId || 'search')
+    var searchField = document.querySelector(searchFieldSelector)
+    var filterInput = searchField.querySelector('.filter input')
+    var controller = docsearch({
+      appId: config.appId,
+      apiKey: config.apiKey,
+      indexName: config.indexName,
+      inputSelector: searchFieldSelector + ' .query',
+      autocompleteOptions: { debug: true, hint: false, keyboardShortcuts: ['s'], minLength: 2 },
+      algoliaOptions: algoliaOptions,
+      queryHook:
+        filterInput &&
+        function (query) {
+          controller.algoliaOptions = filterInput.checked
+            ? Object.assign({}, algoliaOptions, { facetFilters: [filterInput.dataset.facetFilter] })
+            : algoliaOptions
+        },
+    })
+    var eventEmitter = controller.autocomplete
+    var autocomplete = eventEmitter.autocomplete
+    eventEmitter.on('autocomplete:updated', resetScroll.bind(autocomplete.getWrapper().firstChild))
+    if (filterInput) filterInput.addEventListener('change', toggleFilter.bind(controller.input))
+    searchField.addEventListener('click', confineEvent)
+    document.documentElement.addEventListener('click', resetSearch.bind(autocomplete))
+    if (controller.input.attr('autofocus')) controller.input.focus()
+  }
+
+  function appendStylesheet (href) {
+    var link = document.createElement('link')
+    link.rel = 'stylesheet'
+    link.href = href
+    document.head.appendChild(link)
+  }
+
+  function resetScroll () {
+    this.scrollTop = 0
+  }
+
+  function toggleFilter () {
+    this.focus()
+    var dropdown = this.data('aaAutocomplete').dropdown
+    if (!dropdown.isOpen || !this.val()) return
+    dropdown.datasets[0].cachedSuggestions.length = 0
+    dropdown.update(this.val())
+  }
+
+  function confineEvent (e) {
+    e.stopPropagation()
+  }
+
+  function resetSearch () {
+    this.close()
+    this.setVal()
+  }
 })()
