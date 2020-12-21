737f3805ac368ea641086cc3aa5fbefdf3b5004c
Dan Allen <dan.j.allen@gmail.com>
2020-12-20 17:43:48 -0700

    access navigator from window; move call writeText to a helper function and trap exception

--- src/js/07-copy-page-spec.js
+++ src/js/07-copy-page-spec.js
@@ -1,26 +1,34 @@
 ;(function () {
   'use strict'
 
+  if (!window.navigator.clipboard) return
+
+  var HEADING_RX = /H[2-6]/
+
   var pageSpec = (document.querySelector('head meta[name=page-spec]') || {}).content
   var editPageLink = document.querySelector('.toolbar .edit-this-page a')
   if (!(pageSpec && editPageLink)) return
   if (editPageLink) editPageLink.addEventListener('click', onEditPageLinkClick)
   ;[].slice.call(document.querySelectorAll('.doc a.anchor')).forEach(function (anchor) {
-    if (/H[2-6]/.test(anchor.parentNode.tagName)) anchor.addEventListener('click', onSectionAnchorClick.bind(anchor))
+    if (HEADING_RX.test(anchor.parentNode.tagName)) anchor.addEventListener('click', onSectionAnchorClick.bind(anchor))
   })
 
   function onEditPageLinkClick (e) {
-    if (e.altKey) navigator.clipboard.writeText(pageSpec)
+    if (e.altKey) writeToClipboard(pageSpec)
   }
 
   function onSectionAnchorClick (e) {
     if (e.altKey) {
       e.preventDefault()
-      navigator.clipboard.writeText(pageSpec + decodeFragment(this.hash))
+      writeToClipboard(pageSpec + decodeFragment(this.hash))
     }
   }
 
   function decodeFragment (hash) {
     return ~hash.indexOf('%') ? decodeURIComponent(hash) : hash
+  }
+
+  function writeToClipboard (text) {
+    window.navigator.clipboard.writeText(text).then(function () {}, function () {})
   }
 })()
