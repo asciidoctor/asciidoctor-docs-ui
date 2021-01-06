bcf7fe7a0679f5fc16ec53b140cba20f829bb9b8
Dan Allen <dan.j.allen@gmail.com>
2021-01-06 01:00:45 -0700

    pass uiRootPath via script tag instead of global variable

--- src/js/06-copy-to-clipboard.js
+++ src/js/06-copy-to-clipboard.js
@@ -35,12 +35,12 @@
         var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
         svg.setAttribute('class', 'copy-icon')
         var use = document.createElementNS('http://www.w3.org/2000/svg', 'use')
-        use.setAttribute('href', window.uiRootPath + '/img/octicons-16.svg#icon-clippy')
+        use.setAttribute('href', config.uiRootPath + '/img/octicons-16.svg#icon-clippy')
         svg.appendChild(use)
         copy.appendChild(svg)
       } else {
         var img = document.createElement('img')
-        img.src = window.uiRootPath + '/img/octicons-16.svg#view-clippy'
+        img.src = config.uiRootPath + '/img/octicons-16.svg#view-clippy'
         img.alt = 'copy icon'
         img.className = 'copy-icon'
         copy.appendChild(img)
