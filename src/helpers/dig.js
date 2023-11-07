b7d0f61de09682ff296180e7c0b7cde24b26ce8e
Dan Allen <dan@opendevise.com>
2023-11-07 15:17:09 -0700

    update dig to return immediately if object passed in is null

--- src/helpers/dig.js
+++ src/helpers/dig.js
@@ -1,6 +1,7 @@
 'use strict'
 
 module.exports = (object, path) => {
+  if (object == null) return
   const paths = path.split('.')
   let result = object
   for (let i = 0, len = paths.length; i < len; i++) {
